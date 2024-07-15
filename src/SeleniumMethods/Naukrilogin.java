455e584f5ec1974c263242e60afbfd633310802c
Venkata2349 <prasadk2142@gmail.com>
2024-07-15 11:47:21 +0530

    lines are added in the class

--- src/SeleniumMethods/Naukrilogin.java
+++ src/SeleniumMethods/Naukrilogin.java
@@ -1,9 +1,15 @@
 package SeleniumMethods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class Naukrilogin {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		
 
 	}
 
